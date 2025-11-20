b9ba1e0febd27b6096cbd2619ad0a5154b509ae7
sayoorJengkol <62087838+baskoroaji@users.noreply.github.com>
2025-11-20 22:28:15 +0700

    add address route

--- backend/internal/http/route/route.go
+++ backend/internal/http/route/route.go
@@ -8,11 +8,12 @@ import (
 )
 
 type RouteConfig struct {
-	App             *fiber.App
-	HelloController *http.HelloController
-	UserController  *http.UserController
-	AuthController  *http.AuthController
-	AuthMiddleware  *middleware.AuthMiddleware
+	App               *fiber.App
+	HelloController   *http.HelloController
+	UserController    *http.UserController
+	AuthController    *http.AuthController
+	AuthMiddleware    *middleware.AuthMiddleware
+	AddressController *http.AddressController
 }
 
 func (c *RouteConfig) Setup() {
@@ -24,4 +25,9 @@ func (c *RouteConfig) AuthRoute() {
 	// c.App.Get("/hello", c.HelloController.Hello)
 	auth.Post("/register", c.UserController.Register)
 	auth.Post("/login", c.AuthController.Login)
+}
+
+func (c *RouteConfig) AddressRoute() {
+	address := c.App.Group("/v1/api/address")
+	address.Post("/create", c.AuthMiddleware.RequiredAuth(), c.AddressController.Create)
 }
