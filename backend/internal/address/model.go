07d2393c08a173515774a936a4d9882df9067f13
sayoorJengkol <62087838+baskoroaji@users.noreply.github.com>
2025-11-30 21:23:47 +0700

    refactor model, custom validation, custom error

--- backend/internal/address/model.go
+++ backend/internal/address/model.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
 type Address struct {
 	ID         int    `json:"id"`
 	Jalan      string `json:"jalan"`
@@ -15,4 +21,17 @@ type AddressRequest struct {
 	RW         string `json:"RW" validate:"required,RT_RW"`
 	Kota       string `json:"Kota" validate:"required"`
 	PostalCode string `json:"PostalCode" validate:"required,postal_code"`
+}
+
+func CustomRtRwCodeValidation(fl validator.FieldLevel) bool {
+	value := fl.Field().String()
+	match, _ := regexp.MatchString(`^[0-9]{3}$`, value)
+	return match
+
+}
+
+func CustomPostalCodeValidation(fl validator.FieldLevel) bool {
+	postal := fl.Field().String()
+	re_pcode := regexp.MustCompile(`^[1-9][0-9]{4}$`)
+	return re_pcode.MatchString(postal)
 }
---
+++ backend/internal/user/errors.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var Messages = map[string]string{
+	"required":    "%s is required",
+	"email":       "%s must be a valid email address",
+	"min":         "%s must be at least %s characters",
+	"max":         "%s must be at most %s characters",
+	"postal_code": "%s must be exact %s characters",
+	"RT_RW":       "%s must be exact %s characters",
+}
+
+func ValidationError(err error) map[string]string {
+	if err == nil {
+		return nil
+	}
+
+	if validationErrs, ok := err.(validator.ValidationErrors); ok {
+		errors := make(map[string]string)
+		for _, fe := range validationErrs {
+			template := Messages[fe.Tag()]
+			if template == "" {
+				template = "%s is invalid"
+			}
+			if fe.Param() != "" {
+				errors[fe.Field()] = fmt.Sprintf(template, fe.Field(), fe.Param())
+			} else {
+				errors[fe.Field()] = fmt.Sprintf(template, fe.Field())
+			}
+		}
+		return errors
+	}
+
+	return nil
+}
+
+func FormatValidationErrors(errs map[string]string) string {
+	msg := "{"
+	for field, err := range errs {
+		msg += fmt.Sprintf("\"%s\": \"%s\",", field, err)
+	}
+	if len(msg) > 1 {
+		msg = msg[:len(msg)-1]
+	}
+	msg += "}"
+	return msg
+}
