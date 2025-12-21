2760d8cd9121a6c9577d710107aa931270fdc1c3
sayoorJengkol <62087838+baskoroaji@users.noreply.github.com>
2025-12-22 00:30:44 +0700

    fix variable name for address error message

--- sistem-06-Backend/internal/pkg/errors/address_errors.go
+++ sistem-06-Backend/internal/pkg/errors/address_errors.go
@@ -1,31 +1,7 @@
 package errors
 
-var Messages = map[string]string{
+var AddressMessages = map[string]string{
 	"required":    "%s is required",
 	"postal_code": "%s must be exact %s characters",
 	"RT_RW":       "%s must be exact %s characters",
 }
-
-// func ValidationError(err error) map[string]string {
-// 	if err == nil {
-// 		return nil
-// 	}
-
-// 	if validationErrs, ok := err.(validator.ValidationErrors); ok {
-// 		errors := make(map[string]string)
-// 		for _, fe := range validationErrs {
-// 			template := Messages[fe.Tag()]
-// 			if template == "" {
-// 				template = "%s is invalid"
-// 			}
-// 			if fe.Param() != "" {
-// 				errors[fe.Field()] = fmt.Sprintf(template, fe.Field(), fe.Param())
-// 			} else {
-// 				errors[fe.Field()] = fmt.Sprintf(template, fe.Field())
-// 			}
-// 		}
-// 		return errors
-// 	}
-
-// 	return nil
-// }
